3c8366562455ef0b9a2bb6ff99801bc5eeac338b
matilderuiz666 <a2024112013@estudiantes.upsa.edu.bo>
2024-11-13 13:58:15 -0400

    funciones.h

---
+++ funciones.h
@@ -0,0 +1,12 @@
+// funciones.h
+#ifndef FUNCIONES_H  // Directiva de inclusión múltiple
+#define FUNCIONES_H
+
+// Declaraciones de funciones
+void saludar();        // Función que imprime un saludo
+
+int multiplicar(int a, int b); // Función que suma dos números y retorna el resultado
+int restar(int a, int b);
+int suma(int a, int b);
+
+#endif // FUNCIONES_H
